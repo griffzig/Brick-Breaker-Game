a434f8d82767bb76d5a05d87ff64864058795a8d
Landlnni <123333626+Landlnni@users.noreply.github.com>
2023-11-28 15:09:41 -0600

    added more class files

---
+++ brick_breaker/brick_breaker/src/brick_breaker/Paddle.java
@@ -0,0 +1,9 @@
+package brick_breaker;
+
+
+//We could maybe reuse code from bricks to create the paddle. 
+//Making it a controllable brick but don't know how you want to handle it
+
+public class Paddle {
+
+}
